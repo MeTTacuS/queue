b997d018a95fee4b70d35797a88688480b3b8b56
Ned <33838452+MeTTacuS@users.noreply.github.com>
2018-05-08 10:21:03 +0300

    A file for checking, how the queue works

--- queue_user.c
+++ queue_user.c
@@ -8,12 +8,12 @@ int main()
 	tQueue *queue = createQueue(&errorCode);
 	if (checkIfEmpty(queue, &errorCode) == true)
 	{
-		printf("Eile yra tuscia\n");
+		printf("Queue is empty\n");
 	}
 	enQueue(queue, 4, &errorCode);
     enQueue(queue, 3, &errorCode);
 	deQueue(queue, &errorCode);
-	printf("Pirmas queue elementas yra %d\n", getData(queue, &errorCode));
+	printf("First element in queue is %d\n", getData(queue, &errorCode));
     freeQueue(queue);
 }
 
